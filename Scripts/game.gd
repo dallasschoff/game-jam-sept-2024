88e9e8145b0ceb9c1daece2837a8e373f6aa8870
Robert Dallas Schoff <schoffdallas@gmail.com>
2024-09-14 17:00:57 -0700

    Alternative Testing Dimensions

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -4,9 +4,9 @@ extends Node2D
 @onready var main_menu = $CanvasLayer/MainMenu
 
 func _ready():
-	var x = (DisplayServer.screen_get_size().x * 0.625) / 2
-	var y = DisplayServer.screen_get_size().y / 2
-	DisplayServer.window_set_size(Vector2i(x, y))
+	#var x = (DisplayServer.screen_get_size().x * 0.625) / 2
+	#var y = DisplayServer.screen_get_size().y / 2
+	DisplayServer.window_set_size(Vector2i(160 * 5, 144 * 5))
 	main_menu.connect("start_game", start_game)
 
 func start_game():
