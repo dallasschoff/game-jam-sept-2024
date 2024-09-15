b797ced552b9a69f7291faccf3f25cce62839b1f
nvaughan314 <77811880+nvaughan314@users.noreply.github.com>
2024-09-15 00:14:40 -0400

    Holepunch lighting shaders

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,5 +1,17 @@
 extends Node
 
+var player_position : Vector2;
+@onready var player = $Player
+@onready var shader = $ColorRect
+var shaderMat
+
+var material : Shader = load("res://Scenes/lighting.gdshader")
+
+func _ready():
+	shaderMat = shader.material
+	#shaderMat.set_shader_parameter("player_position",player_position)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	player_position = player.position
+	shaderMat.set_shader_parameter("player_position",player_position)
