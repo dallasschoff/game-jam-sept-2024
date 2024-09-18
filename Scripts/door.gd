bb95ec9562044fc8188ba514d7dc90216f2cf8c8
nvaughan314 <77811880+nvaughan314@users.noreply.github.com>
2024-09-18 00:28:44 -0400

    Door sound implement

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -8,5 +8,8 @@ func _ready():
 	totem.connect("totemActivate", _open)
 
 func _open():
-	collision.set_deferred("disabled", true)
+	await get_tree().create_timer(1.8).timeout
+	$DoorOpenSound.play()
 	play("open")
+	await get_tree().create_timer(1).timeout
+	collision.set_deferred("disabled", true)
