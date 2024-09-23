745dba013ed4f3ead2548a8535b4fc6f96888cbf
Robert Dallas Schoff <schoffdallas@gmail.com>
2024-09-22 20:59:23 -0700

    Light Dimming for Burnables

--- Scripts/branches.gd
+++ Scripts/branches.gd
@@ -1,17 +1,26 @@
 extends AnimatedSprite2D
+class_name Branches
 
 var lit = false
 @export var start_frame : float = 0.0
-@export var burn_radius : float = 40.0
+@export var max_burn_radius : float = 40.0
 @export var glow_speed : float = 2.0
+var fade_light = false
+var burn_radius = -10.0
+@onready var collision = $StaticBody2D/CollisionShape2D
 
 func _ready():
 	add_to_group("Burnables")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if lit:
+	if lit and not fade_light and burn_radius < max_burn_radius:
 		play("burning")
+		burn_radius += 0.2
+	if fade_light and burn_radius > 0:
+		burn_radius -= 0.25
+	elif fade_light and burn_radius < 0:
+		queue_free()
 
 func _on_area_2d_area_entered(area):
 	if area.get_parent() is Ripple:
@@ -19,4 +28,6 @@ func _on_area_2d_area_entered(area):
 		start_frame = Engine.get_process_frames()
 
 func _on_burn_animation_finished():
-	queue_free()
+	fade_light = true
+	collision.set_deferred("disabled", true)
+	
--- Scripts/totem.gd
+++ Scripts/totem.gd
@@ -5,23 +5,28 @@ signal totemActivate
 
 var lit = false
 @export var start_frame : float = 0.0
-@export var burn_radius : float = 30.0
+@export var max_burn_radius : float = 30.0
 @export var glow_speed : float = 4.0
 var firstActivated : bool = false
+var burn_radius = -10.0
+var fade_light = false
 
 func _ready():
 	play("default")
 	add_to_group("Burnables")
 	add_to_group("DoorTotems")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
 func _process(delta):
-	pass
+	if lit and not fade_light and burn_radius < max_burn_radius:
+		burn_radius += 0.25
+	elif not lit and fade_light and burn_radius > -10.0:
+		burn_radius -= 0.1
 
 #When a pulse enters a totem's area2d, the totem should be lit
 func _on_area_2d_area_entered(area):
 	if lit == false:
 		lit = true
+		fade_light = false
 		start_frame = Engine.get_process_frames()
 		play("start_burning")
 		if not firstActivated:
@@ -31,8 +36,9 @@ func _on_area_2d_area_entered(area):
 #When an area2d exits (despawns) the totem's area2d, the totem should extinguish
 func _on_area_2d_area_exited(area):
 	if lit == true:
-		await get_tree().create_timer(5).timeout
+		await get_tree().create_timer(1).timeout
 		lit = false
+		fade_light = true
 		play("reverse_start_burning")
 
 func _on_animation_finished():
