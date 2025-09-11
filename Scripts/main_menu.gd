41a46c102496387fafe9ed7230aaf80bafa65331
Krieglliam <billyjr129@gmail.com>
2025-09-10 21:58:37 -0400

    4.4 changes and some work on mobile UI

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -2,15 +2,24 @@ extends Control
 
 signal start_game
 var levelOne: PackedScene = load("res://Scenes/Level.tscn")
+var mobileControls
 
-@onready var start_button = $MarginContainer/VBoxContainer/VBoxContainer/Start
+@onready var start_button = $MarginContainer/VBoxContainer2/Start
 
 func _ready():
 	DisplayServer.window_set_size(Vector2i(160 * 5, 144 * 5))
 	start_button.grab_focus()
+	Global.mainMenu = self
 
 func _on_start_pressed():
 	get_tree().change_scene_to_packed(levelOne)
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		mobileControls = true
+	if !toggled_on:
+		mobileControls = false
